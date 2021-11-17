96ec563b02eb468736c14ac93efa5fce52525600
Sagar Vora <sagar@resilient.tech>
2021-11-17 15:44:49 +0530

    fix: move commit call to `mark_email_as_seen`
    (cherry picked from commit 7b7f74ce23c2d9af7d5d6ee01e209385778aa51c)

--- frappe/core/doctype/communication/email.py
+++ frappe/core/doctype/communication/email.py
@@ -494,6 +494,7 @@ def sendmail(communication_name, print_html=None, print_format=None, attachments
 def mark_email_as_seen(name: str = None):
 	try:
 		update_communication_as_seen(name)
+		frappe.db.commit()  # nosemgrep: this will be called in a GET request
 
 	except Exception:
 		frappe.log_error(frappe.get_traceback())
@@ -530,5 +531,3 @@ def update_communication_as_seen(name):
 		"delivery_status": "Read",
 		"read_by_recipient_on": get_datetime()
 	})
-
-	frappe.db.commit()

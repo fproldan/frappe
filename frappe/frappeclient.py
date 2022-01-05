9889ed492cb8dea1ece48b2b7feec49021a8f25f
Gavin D'souza <gavin18d@gmail.com>
2022-01-05 20:37:33 +0530

    fix: Use params instead of data for client login

--- frappe/frappeclient.py
+++ frappe/frappeclient.py
@@ -1,13 +1,13 @@
-from __future__ import print_function, unicode_literals
-import requests
-import json
-import frappe
-from six import iteritems, string_types
-import base64
-
 '''
 FrappeClient is a library that helps you connect with other frappe systems
 '''
+import base64
+import json
+
+import requests
+
+import frappe
+
 
 class AuthError(Exception):
 	pass
@@ -48,7 +48,7 @@ class FrappeClient(object):
 
 	def _login(self, username, password):
 		'''Login/start a sesion. Called internally on init'''
-		r = self.session.post(self.url, data={
+		r = self.session.post(self.url, params={
 			'cmd': 'login',
 			'usr': username,
 			'pwd': password
@@ -288,15 +288,17 @@ class FrappeClient(object):
 		doc.modified = frappe.db.get_single_value(doctype, "modified")
 		frappe.get_doc(doc).insert()
 
-	def get_api(self, method, params={}):
-		res = self.session.get(self.url + "/api/method/" + method + "/",
+	def get_api(self, method, params=None):
+		if params is None:
+			params = {}
+		res = self.session.get(f"{self.url}/api/method/{method}",
 			params=params, verify=self.verify, headers=self.headers)
 		return self.post_process(res)
 
 	def post_api(self, method, params=None):
 		if params is None:
 			params = {}
-		res = self.session.post(self.url + "/api/method/" + method,
+		res = self.session.post(f"{self.url}/api/method/{method}",
 			params=params, verify=self.verify, headers=self.headers)
 		return self.post_process(res)
 

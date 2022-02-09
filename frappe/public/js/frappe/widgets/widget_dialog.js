6da5dd67d6b0919e151429ee27a4875cb56a43c0
Francisco Roldan <franciscoproldan@gmail.com>
2022-02-09 11:36:13 -0300

    fix: colors

--- frappe/public/js/frappe/widgets/widget_dialog.js
+++ frappe/public/js/frappe/widgets/widget_dialog.js
@@ -31,11 +31,8 @@ class WidgetDialog {
 	}
 
 	get_title() {
-		// DO NOT REMOVE: Comment to load translation
-		// __("New Chart") __("Add Chart") __("New Shortcut") __("Edit Chart") __("Edit Shortcut") __("Add Shortcut")
-		let action = this.editing ? "Edit" : "Add";
-		let label = action = action + " " + frappe.model.unscrub(this.type);
-		return __(label);
+		let action = this.editing ? __("Edit") : __("Add");
+		return action + " " + __(frappe.model.unscrub(this.type));
 	}
 
 	get_fields() {

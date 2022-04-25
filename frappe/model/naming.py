1218e7cc8785445b7bfba630e9accb431f5ec550
Francisco Roldán <franciscoproldan@gmail.com>
2022-04-25 12:26:56 -0300

    Backport 117 to diamoerp staging (#130)
    * Asignar nombre luego de validar  (#117)
    
    * feat: set_name_after_submit
    
    * feat: set_name_after_submit
    
    * fix: doctype js
    
    * Asignar nombre luego de validar  (#117)
    
    * feat: set_name_after_submit
    
    * feat: set_name_after_submit
    
    * fix: doctype js

--- frappe/model/naming.py
+++ frappe/model/naming.py
@@ -46,8 +46,8 @@ def set_new_name(doc, set_draft_name=False):
 		doc.name = None
 
 	if getattr(doc, "amended_from", None):
-		_set_amended_name(doc)
-	  return
+		doc.name = _get_amended_name(doc)
+		return
 
 	elif getattr(doc.meta, "issingle", False):
 		doc.name = doc.doctype

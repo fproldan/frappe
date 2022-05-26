4b503acb384b0686e09a4c72c1ec61f039890c06
Francisco Roldan <franciscoproldan@gmail.com>
2022-05-26 12:14:23 -0300

    command

--- frappe/commands/delete_sensitive_data.py
+++ frappe/commands/delete_sensitive_data.py
@@ -1,4 +1,15 @@
 import frappe
 
 def delete_sensitive_data():
-	print("Eliminando...")
+    installed_apps = frappe.get_installed_apps()
+
+    if "erpnext_argentina" in installed_apps:
+        for company in frappe.get_all("Company", pluck="name"):
+            frappe.db.set_value("Company", company, "certificado_publico", "")
+            frappe.db.set_value("Company", company, "certificado_privado", "")
+            frappe.db.set_value("Company", company, "tipo_de_conexion_afip", "Homologacion")
+            frappe.db.commit()
+
+
+    if "ecommerce_integrations" in installed_apps:
+        print("TODO disable sync")

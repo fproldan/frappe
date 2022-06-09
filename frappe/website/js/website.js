decae4933276d7b3dc626ef71087cc16c61c8c0e
Francisco Roldan <franciscoproldan@gmail.com>
2022-06-09 11:22:03 -0300

    fix: search

--- frappe/website/js/website.js
+++ frappe/website/js/website.js
@@ -444,7 +444,7 @@ frappe.setup_search = function (target, search_scope) {
 	}
 
 	let $search_input = $(`<div class="dropdown" id="dropdownMenuSearch">
-			<input type="search" class="form-control" placeholder="Search the docs (Press / to focus)" />
+			<input type="search" class="form-control" placeholder="Buscar en la documentación (Pulse / para enfocar)" />
 			<div class="overflow-hidden shadow dropdown-menu w-100" aria-labelledby="dropdownMenuSearch">
 			</div>
 			<div class="search-icon">

c3d6cef24220d17792efeb56712bd3d34a00e028
fproldan <franciscoproldan@gmail.com>
2022-10-13 09:30:43 -0300

    fix mark as seen

--- frappe/email/queue.py
+++ frappe/email/queue.py
@@ -541,7 +541,8 @@ def prepare_message(email, recipient, recipients_list):
 	email_account = get_outgoing_email_account(raise_exception_not_set=False, sender=email.sender)
 	if frappe.conf.use_ssl and email_account.track_email_status:
 		# Using SSL => Publically available domain => Email Read Reciept Possible
-		message = message.replace("<!--email open check-->", quopri.encodestring('<img src="https://{}/api/method/frappe.core.doctype.communication.email.mark_email_as_seen?name={}"/>'.format(frappe.local.site, email.communication).encode()).decode())
+		site = frappe.conf.host_name or frappe.local.site
+		message = message.replace("<!--email open check-->", quopri.encodestring('<img src="https://{}/api/method/frappe.core.doctype.communication.email.mark_email_as_seen?name={}"/>'.format(site, email.communication).encode()).decode())
 	else:
 		# No SSL => No Email Read Reciept
 		message = message.replace("<!--email open check-->", quopri.encodestring("".encode()).decode())

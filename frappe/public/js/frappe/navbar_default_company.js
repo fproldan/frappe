f7dfd3fd020d28e208c9449dd647dbe9a3e95a70
fproldan <franciscoproldan@gmail.com>
2022-10-27 11:15:13 -0300

    fix

--- frappe/public/js/frappe/navbar_default_company.js
+++ frappe/public/js/frappe/navbar_default_company.js
@@ -4,7 +4,7 @@ $(function() {
 	$('#navbar-default-company').text(company);
 
 	frappe.db.get_value('Company', company, 'color', (values) => {
-		let value = "2px solid " + values.color;
+		let value = "3px solid " + values.color;
 		$('#navbar-default-company-item').css('border-bottom', value);
 	});
 });

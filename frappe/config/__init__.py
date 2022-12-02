01fef76f2d10dadfa68eac9f4b54aa348f864019
fproldan <franciscoproldan@gmail.com>
2022-12-02 14:23:21 -0300

    hide

--- frappe/config/__init__.py
+++ frappe/config/__init__.py
@@ -41,7 +41,7 @@ def get_modules_from_app(app):
 		'Healthcare', 'Setup', 'Manufacturing', 'Restaurant', 'Hotels', 'Communication', 'E-commerce',
 		'Core', 'Email', 'Geo', 'Desk', 'Printing', 'Contacts', 'Data Migration', 'Chat', 'Social',
 		'ERPNext Integrations', 'Frappe Chat', 'Zenoti', 'unicommerce', 'Amazon', 'Argentina',
-		'Custom', 'Portal', 'Ecommerce', 'Workflow', 'Ecommerce Integrations'
+		'Custom', 'Portal', 'Ecommerce', 'Workflow', 'Ecommerce Integrations', 'shopify'
 	]
 	return [m for m in frappe.get_all('Module Def',
 		filters={'app_name': app},

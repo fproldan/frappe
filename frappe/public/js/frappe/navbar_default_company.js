aa6bbdeb5e2840f405c86dd39b1c9a543959f2d2
fproldan <franciscoproldan@gmail.com>
2023-02-08 12:00:01 -0300

    fix

--- frappe/public/js/frappe/navbar_default_company.js
+++ frappe/public/js/frappe/navbar_default_company.js
@@ -1,10 +1,12 @@
 $(function() {
 	var company = frappe.defaults.get_user_default('company');
 
-	$('#navbar-default-company').text(company);
+	if (company) {
+		$('#navbar-default-company').text(company);
 
-	frappe.db.get_value('Company', company, 'color', (values) => {
-		let value = "3px solid " + values.color;
-		$('#navbar-default-company-item').css('border-bottom', value);
-	});
+		frappe.db.get_value('Company', company, 'color', (values) => {
+			let value = "3px solid " + values.color;
+			$('#navbar-default-company-item').css('border-bottom', value);
+		});
+	}
 });
